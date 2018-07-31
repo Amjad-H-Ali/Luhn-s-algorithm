c43006441452d5a6ff0216172ac5ae757b4a7bbc
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-07-31 05:20:31 -0500

    Program prompts user for cc number  until it meets specifications

--- JS/credit.js
+++ JS/credit.js
@@ -1,1 +1,12 @@
 console.log('Luhn\'s Algo: Credit.');
+
+let ccNum;
+
+// Get cc number from user.
+do {
+	ccNum = prompt();
+}
+
+// CC number has to be a non-negative integer
+while (ccNum < 0 || isNaN(parseInt(ccNum)) || isNaN(Number(ccNum)) || parseInt(ccNum) != ccNum);
+
