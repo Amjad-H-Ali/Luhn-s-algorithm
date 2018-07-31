a0d28a34b2879be472ff2d9d817db89f77acee13
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-07-31 05:35:05 -0500

    Wrote a function that gets length of a number

--- JS/credit.js
+++ JS/credit.js
@@ -10,3 +10,9 @@ do {
 // CC number has to be a non-negative integer
 while (ccNum < 0 || isNaN(parseInt(ccNum)) || isNaN(Number(ccNum)) || parseInt(ccNum) != ccNum);
 
+ // Function to Get Length of a number
+const getNumLen = (num) => {
+	return parseInt(num) > 0 ? 1 + getNumLen(num/10) : 0; 
+};
+
+console.log(getNumLen(ccNum));
