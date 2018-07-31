3872a2e1bb5869d7f1c3a8d82fa9f0f550c8893b
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-07-31 05:53:06 -0500

    Added a function that gets every other digit starting from the second-to-last digit in the number

--- JS/credit.js
+++ JS/credit.js
@@ -15,4 +15,18 @@ const getNumLen = (num) => {
 	return parseInt(num) > 0 ? 1 + getNumLen(num/10) : 0; 
 };
 
-console.log(getNumLen(ccNum));
+// Function that stores every other digit starting from second-to-last digit in number.
+const everyOtherDigit = (num) => {
+	let num_arr = [];
+	// Modulous 10 get last digit of num. We need to divide by 10 to get second-to-last digit.
+	let denom = 10;
+	// Increase the denominator by 100 to get every other number.
+	// We only need to loop for half the length of num because there are only that amnt of "other" digits
+	for (let i = 0, len = Math.floor(getNumLen(num)/2); i < len; i ++) {
+		num_arr.push(parseInt((num/denom) % 10));
+		denom *= 100;
+	};
+	return num_arr;
+};
+
+console.log(everyOtherDigit(ccNum));
